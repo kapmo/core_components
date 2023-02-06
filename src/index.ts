4e9ee851954bfd79d6bc5a7450347cca2bf3498d
kapmo <monu.kaparthi@gmail.com>
2023-02-06 12:11:15 +0530

    removed unused components

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import Button from "./components/Button/Button";
-import Input from "./components/Input/Input";
+
 import * as InterfaceProps from './interfaces';
 import CustomTable from "./components/CustomTable/CustomTable";
 
-export { Button }
-export { Input }
-// export { InterfaceProps }
-export { CustomTable }
+export { InterfaceProps, CustomTable }
 
